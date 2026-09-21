c31e2b021f20444cdcce5abcb8ca8541a6f78256
thibautvas <thibaut.vas@gmail.com>
2026-09-21 19:02:29 +0200

    chore: improve flake
    - keep only plugin build in root flake
    - ext wrappers for nvim and docker to nix subdir
    - align naming of apps and packages
    - add result to gitignore
    - update lockfile

--- flake.nix
+++ flake.nix
@@ -9,9 +9,7 @@
       inherit (nixpkgs) lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
 
-    in
-    {
-      packages = forAllSystems (
+      perSystem =
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
@@ -21,93 +19,33 @@
             src = ./.;
           };
 
-          luaRcContent = ''
-            vim.g.mapleader = " "
-            vim.o.wrap = false
-            vim.o.number = true
-            vim.o.cursorline = true
-            vim.keymap.set("n", "-", function()
-              local dir = vim.api.nvim_buf_get_name(0) ~= "" and "%:h" or "."
-              vim.cmd("e " .. dir)
-            end)
-          '';
+          wrappedNvim = pkgs.callPackage ./nix/nvim.nix {
+            inherit trino-nvim;
+          };
 
-          fromLua = config: ''
-            lua << EOF
-            ${config}
-            EOF
-          '';
-
-          plugins = [
-            {
-              plugin = trino-nvim;
-              config = fromLua ''
-                require("trino").setup({
-                  host = "localhost",
-                  port = 8080,
-                  catalog = "tpch",
-                  http_headers = { ["X-Trino-Original-User"] = "placeholder" },
-                  http_scheme = "http",
-                  auth = {
-                    type = "none",
-                    username = nil,
-                    password = nil,
-                  },
-                  verify = false,
-                })
-
-                vim.keymap.set("v", "<leader>ef", require("trino").run_visual)
-              '';
-            }
-          ];
-
-          wrapperArgs =
-            let
-              extraPkgs = [
-                (pkgs.python3.withPackages (ps: [
-                  (ps.trino-python-client.overridePythonAttrs (oldAttrs: {
-                    pname = "trino";
-                  }))
-                ]))
-              ];
-            in
-            [
-              "--prefix"
-              "PATH"
-              ":"
-              (pkgs.lib.makeBinPath extraPkgs)
-            ];
+          dockerTrino = pkgs.callPackage ./nix/docker.nix { };
 
         in
         {
-          default = trino-nvim;
-          nvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
-            inherit luaRcContent plugins wrapperArgs;
+          packages = {
+            default = trino-nvim;
+            nvim = wrappedNvim;
           };
-        }
-      );
+          apps = {
+            nvim = {
+              type = "app";
+              program = "${wrappedNvim}/bin/nvim";
+            };
+            dockerTrino = {
+              type = "app";
+              program = "${dockerTrino}/bin/${dockerTrino.name}";
+            };
+          };
+        };
 
-      apps = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          dockerTrino = pkgs.writeShellApplication {
-            name = "run-trino";
-            runtimeInputs = [ pkgs.docker ];
-            text = "docker run -d --name trino -p 8080:8080 trinodb/trino";
-          };
-
-        in
-        {
-          default = {
-            type = "app";
-            program = "${self.packages.${system}.nvim}/bin/nvim";
-          };
-          dockerTrino = {
-            type = "app";
-            program = "${dockerTrino}/bin/${dockerTrino.name}";
-          };
-        }
-      );
+    in
+    {
+      packages = forAllSystems (system: (perSystem system).packages);
+      apps = forAllSystems (system: (perSystem system).apps);
     };
 }
